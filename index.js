ad1562ed602a2cf7bcfa9a24578f97098509b3ee
Adrian Mong <mongklhk@gmail.com>
2026-07-05 02:53:58 +0800

    Fixing Vercel Deploy

--- index.js
+++
@@ -1,7 +0,0 @@
-const { spawn } = require('child_process');
-
-const next = spawn('npm', ['run', 'start'], { stdio: 'inherit', shell: true });
-
-next.on('close', (code) => {
-  process.exit(code);
-});
